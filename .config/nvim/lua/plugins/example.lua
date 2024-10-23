cd98a6ebbb4962450d83f72ea6f1eaf7d4f76479
rainbowball <sun_yasong@outlook.com>
2024-10-23 17:05:45 +0800

    Update example.lua

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -7,4 +7,26 @@ return {
       colorscheme = "tokyonight-moon",
     },
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-f>"] = require("telescope.actiions").preview_scrolling_left,
+          },
+        },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    keys = {
+      { "gh", "K", desc = "Hover", remap = true },
+      { "gH", "gK", desc = "Signature Help", remap = true },
+      opts = {
+        inlay_hints = { enabled = false },
+      },
+    },
+  },
 }
