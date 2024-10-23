2e81a171ac3e9d2f2cba24a4d3b7aaa30f00fc76
rainbowball <sun_yasong@outlook.com>
2024-10-23 16:54:01 +0800

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
+
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("o", "L", "$")
+vim.keymap.set("o", "H", "^")
 vim.keymap.set("i", "<M-.>", "<C-r>0", { desc = "register [0]" })
